8f3676420778d1d660b168268d130ca2c99631e5
ole-ya <112708213+ole-ya@users.noreply.github.com>
2025-05-06 11:44:13 +0200

    Add streaming examples

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
+import time
+from datetime import datetime
+
 import grpc
 
 from proto.hello_pb2_grpc import HelloStub
 from proto.hello_pb2 import HelloRequest, HelloResponse
+
 
 class RequestIterator:
     def __init__(self, sequence):
@@ -23,17 +27,41 @@ class RequestIterator:
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = HelloStub(channel)
+        print("\nSimple RPC:")
         response: HelloResponse = stub.SayHello(HelloRequest(name="Oleg"))
-        print("\nSimple RPC: Name: " + response.name + ", Age: " + str(response.age))
+        print("\tSimple RPC response: %s at %s" % (response.name, datetime.now().strftime("%H:%M:%S")))
+
         ### Add streaming response
-        # print("\nResponse streaming RPC: ")
-        # for response in stub.SayHelloResponseStream(HelloRequest(name="Oleg")):
-        #     print("\tName: " + response.name + ", Age: " + str(response.age))
+        print("\nResponse streaming RPC: ")
+        response_stream = stub.SayHelloResponseStream(HelloRequest(name="Oleg"))
+        for r in response_stream:
+            print("\tServer streaming response: %s, at %s" % (r.name, datetime.now().strftime("%H:%M:%S")))
+
         ### Add streaming request
         print("\nRequest streaming RPC: ")
-        print(stub.SayHelloRequestStream([HelloRequest(name="Oleg")]))
-        # Add bidirectional RPC request
-        print("Bidirectional RPC: ")
+
+        def server_requests():
+            for i in range(5):
+                if i not in (2, 4):
+                    yield HelloRequest(name="Oleg (client:%s)" % i)
+                time.sleep(1)
+
+        response = stub.SayHelloRequestStream(server_requests())
+        print("\tClient streaming response: %s, at %s" % (response.name, datetime.now().strftime("%H:%M:%S")))
+
+        ### Add bidirectional RPC request
+        print("\nBidirectional RPC: ")
+
+        def server_requests():
+            for i in range(5):
+                if i not in (1, 3):
+                    yield HelloRequest(name="Oleg (client:%s)" % i)
+                time.sleep(1)
+
+        response_iterator = stub.SayHelloBidirectionalStream(server_requests())
+        for response in response_iterator:
+            print("\tBidirectional response: %s, at %s" % (response.name, datetime.now().strftime("%H:%M:%S")))
+
 
 if __name__ == '__main__':
     run()
--- services/HelloService.py
+++ services/HelloService.py
@@ -1,5 +1,6 @@
-from time import sleep
-from typing import List
+import time
+from datetime import datetime
+from threading import Thread
 
 from proto.hello_pb2_grpc import HelloServicer
 from proto.hello_pb2 import HelloRequest
@@ -9,29 +10,41 @@ from proto.hello_pb2 import HelloResponse
 class HelloService(HelloServicer):
 
     def SayHello(self, request: HelloRequest, context):
+        print("\nSimple request received")
         return HelloResponse(name="Oleg", age=32)
 
     def SayHelloResponseStream(self, request: HelloResponse, context):
-        for name_idx in range(1, 5):
-            sleep(1)
-            yield HelloResponse(name="Oleg {name}".format(name=str(name_idx)), age=32)
+        print("\nServer streaming request received")
+        for idx in range(3):
+            yield HelloResponse(name="Oleg (server:%s)" % idx, age=32)
+            time.sleep(1)
 
     def SayHelloRequestStream(self, request_iterator, context):
-        print(request_iterator)
-        # request_count = 0
-        # request_name = None
-        #
-        for name_req in request_iterator:
-            print(name_req)
-        #     request_count += 1
-        #     request_name = name_req
-        return HelloResponse(name="{name} {idx}".format(name="Foo", idx=1), age=32)
+        print("\nClient streaming request received")
+        response_names = []
 
-    def SayHelloBidirectionalStream(self, request_iterator: List[HelloResponse], context):
-        prev_requests = []
-        for name_req in request_iterator:
-            for prev_request in prev_requests:
-                if prev_request.name == name_req.name:
-                    yield prev_request
-            prev_requests.append(name_req)
+        for idx, i in enumerate(request_iterator):
+            print("\tReceived request for %s at %s" % (i.name, datetime.now().strftime("%H:%M:%S")))
+            response_names.append("%s (server:%i)" % (i.name, idx))
 
+        return HelloResponse(name="; ".join(response_names), age=32)
+
+    def SayHelloBidirectionalStream(self, request_iterator, context):
+        print("\nBidirectional streaming request received")
+        received_requests = []
+
+        # Open thread to parse data, although parsing
+        # can be done in the same thread as well
+        def parse_request():
+            for request in request_iterator:
+                print("\tReceived request for %s at %s" % (request.name, datetime.now().strftime("%H:%M:%S")))
+                received_requests.append(request)
+
+        t = Thread(target=parse_request())
+        t.start()
+
+        for idx, i in enumerate(received_requests):
+            yield HelloResponse(name="%s (server:%s)" % (i.name, idx), age=32)
+            time.sleep(1)
+
+        t.join()
